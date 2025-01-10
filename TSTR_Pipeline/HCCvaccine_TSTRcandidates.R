c43962b1ac111a4b667fb0b9f3ac0a4c4c87e56c
MartaEspinosa <espinosacamarenamarta@gmail.com>
2025-01-10 13:57:02 +0100

    v47

--- TSTR_Pipeline/HCCvaccine_TSTRcandidates.R
+++ TSTR_Pipeline/HCCvaccine_TSTRcandidates.R
@@ -18,9 +18,15 @@ HCC_log2ratio = read.csv("/users/genomics/marta/TestisProject_SaraRazquin/with_T
 HCC_log2ratio = HCC_log2ratio %>% select(gene_id, log2ratio)
 
 ## other immuno
+# by peptide
 immuno = read.csv("/projects_eg/projects/marta/TestisRestricted_Microproteins_TSA/with_TranscriptomeReconstruction/Multimap_altORFs/Q5_immunopeptidomics/human/TOv3x_5percent_TestisRestrictedGTEx_Translated_Ctypes_log2ratio3x_immuno.csv")
-immuno = immuno %>% select(Peptide, gene_id_x, source, matching_ORFpep)
-names(immuno) = c("Peptide_broadSources","gene_id","BroadSource","ORFpep")
+immuno = immuno %>% select(Peptide, gene_id_x, geneORFtype, source, matching_ORFpep)
+names(immuno) = c("Peptide_broadSources","gene_id","geneORFtype", "BroadSource","immuno_ORFpep")
+
+# by gene
+# immuno = read.csv("/projects_eg/projects/marta/TestisRestricted_Microproteins_TSA/with_TranscriptomeReconstruction/Multimap_altORFs/Q5_immunopeptidomics/human/TOv3x_5percent_TestisRestrictedGTEx_Translated_Ctypes_log2ratio3x_immunobyGene.csv")
+# immuno = immuno %>% select(Peptide, gene_id, geneORFtype, source)
+# names(immuno) = c("Peptide_broadSources","gene_id","geneORFtype", "BroadSource")
 
 # ## genes
 # candidates = read.csv(file.path(tumorReactDIR,"TOv3x_5percent_TestisRestrictedGTEx_Translated_Ctypes_log2ratio3xMEANgenes.csv"))
@@ -62,7 +68,8 @@ names(candidatesORFs_pancancer)[2] = "num_ctypes"
 candORFs_HCC = merge(candidatesORFs_HCC, candidatesORFs_pancancer, on="gene_id")
 candORFs_HCC_SciAdv = merge(candORFs_HCC, SciAdv, on="gene_id", all.x=T)
 candORFs_HCC_SciAdv = merge(candORFs_HCC_SciAdv, HCC_log2ratio, on="gene_id", all.x=T)
-candORFs_HCC_SciAdv = merge(candORFs_HCC_SciAdv, immuno, on=c("gene_id","ORFpep"), all.x=T)
+# candORFs_HCC_SciAdv = merge(candORFs_HCC_SciAdv, immuno, on=c("gene_id","ORFpep"), all.x=T)
+candORFs_HCC_SciAdv = merge(candORFs_HCC_SciAdv, immuno, on=c("gene_id","geneORFtype"), all.x=T)
 candORFs_HCC_SciAdv = merge(candORFs_HCC_SciAdv, stats_TSTR, on="gene_id", all.x=T)
 
 ## add immunopeptidomics
@@ -89,7 +96,7 @@ Lof_5percent = Lof_5percent %>% select(sequence, sample, accessions, gene_id, so
 MHCquant = rbind(celllines_5percent, BeijerPT_5percent, Beijer_5percent, Lof_5percent)
 
 candORFs_HCC_SciAdv_MHCq = merge(candORFs_HCC_SciAdv, MHCquant, all.x=T, on="gene_id")
-candORFs_HCC_SciAdv_MHCq = candORFs_HCC_SciAdv_MHCq %>% select(gene_id, gene_name, percentage_num_patients_overexpr, n_SciAdv, log2ratio, coding_noncoding_chr, source, sequence, BroadSource, Peptide_broadSources, orfID, accessions, ORFpep, everything()) %>% unique()
+candORFs_HCC_SciAdv_MHCq = candORFs_HCC_SciAdv_MHCq %>% select(gene_id, gene_name, geneORFtype, percentage_num_patients_overexpr, n_SciAdv, log2ratio, coding_noncoding_chr, source, sequence, BroadSource, Peptide_broadSources, orfID, accessions, ORFpep, everything()) %>% unique()
 write.csv(candORFs_HCC_SciAdv_MHCq, "/home/marta/candORFs_HCC_SciAdv_MHCq.csv", row.names = F)
 
 ### lncRNAs
